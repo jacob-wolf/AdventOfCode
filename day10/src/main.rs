e840e15692bb0b1358cabc4c9204e00fa447962d
Jacob Wolf <jaketwolf1113@gmail.com>
2023-12-11 18:08:05 -0600

    day 10 solution

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs::read_to_string, arch::x86_64::_mm_loaddup_pd};
+use std::{collections::HashSet, fs::read_to_string};
 
 fn main() {
     println!("{:?}", part1(&"input.txt"));
@@ -117,7 +117,7 @@ fn part1(path: &str) -> usize {
 
 fn part2(path: &str) -> usize {
     let file = read_to_string(&path).unwrap();
-    let map = file
+    let mut map = file
         .lines()
         .map(|line| line.chars().collect::<Vec<char>>())
         .collect::<Vec<Vec<char>>>();
@@ -131,10 +131,11 @@ fn part2(path: &str) -> usize {
 
     //to make a loop needs to connect on two sides, reviewed inputs to pick start direction and neglect any overflow errors
 
-    let mut curr_position = if is_step_valid(
-        &map[starting_coordinate.0 - 1][starting_coordinate.1],
-        &StepDirection::Up,
-    ) {
+    let mut curr_position = if starting_coordinate.0 != 0
+        && is_step_valid(
+            &map[starting_coordinate.0 - 1][starting_coordinate.1],
+            &StepDirection::Up,
+        ) {
         previous_step_direction = StepDirection::Up;
         (starting_coordinate.0 - 1, starting_coordinate.1)
     } else if is_step_valid(
@@ -202,18 +203,80 @@ fn part2(path: &str) -> usize {
         };
         previous_step_direction = next_step_direction;
     }
-    let max_x = loop_coordinates.iter().max().unwrap().0;
-    let min_x = loop_coordinates.iter().min().unwrap().0;
 
-    let max_y = loop_coordinates.iter().map(|(_,y)| y).max().unwrap();
-    let min_y = loop_coordinates.iter().map(|(_,y)| y).min().unwrap();
-    
-    let x_extent_internal = max_x - min_x - 2;
-    let y_extent_internal = max_y - min_y - 2;
+    let s_replacement = determine_s_replacement(&starting_coordinate, &map);
+    map[s_row_index][s_col_index] = s_replacement;
 
+    let num_cols = map[0].len();
+    let num_rows = map.len();
+    let loop_set: HashSet<(usize, usize)> = HashSet::from_iter(loop_coordinates);
+    let mut internal_count: usize = 0;
+    for row_index in 0..num_rows {
+        let mut is_inside = false;
+        let mut opening_pair: Option<char> = None;
 
-    let corner_char_count = loop_coordinates.iter().map(|(row, col)| map[*row][*col]).filter(|c| ['7', 'F', 'J', 'L'].contains(c)).collect::<Vec<char>>().len() - 4;
-    
-    y_extent_internal*x_extent_internal - corner_char_count.div_euclid(2)
+        for col_index in 0..num_cols {
+            if loop_set.contains(&(row_index, col_index)) {
+                //check if need to update inside status
+                if map[row_index][col_index] == '|' {
+                    is_inside = !is_inside;
+                } else {
+                    if let None = opening_pair {
+                        if ['F', 'L'].contains(&map[row_index][col_index]) {
+                            opening_pair = Some(map[row_index][col_index]);
+                        }
+                    } else {
+                        if ['J', '7'].contains(&map[row_index][col_index]) {
+                            let opener = opening_pair.unwrap();
+                            if opener == 'F' && map[row_index][col_index] == 'J' {
+                                is_inside = !is_inside;
+                            } else if opener == 'L' && map[row_index][col_index] == '7' {
+                                is_inside = !is_inside;
+                            }
+                            opening_pair = None;
+                        }
+                    }
+                }
+            } else {
+                if is_inside {
+                    internal_count += 1;
+                }
+            }
+        }
+    }
+    internal_count
+}
 
+fn determine_s_replacement(starting_coordinate: &(usize, usize), map: &Vec<Vec<char>>) -> char {
+    if ['7', 'F', '|'].contains(&map[starting_coordinate.0 - 1][starting_coordinate.1]) {
+        // up is valid
+        if ['|', 'J', 'L'].contains(&map[starting_coordinate.0 + 1][starting_coordinate.1]) {
+            //down is valid
+            return '|';
+        } else {
+            //down is invalid
+            if ['J', '7', '-'].contains(&map[starting_coordinate.0][starting_coordinate.1 + 1]) {
+                //right is valid
+                return 'L';
+            } else {
+                //left is valid
+                return 'J';
+            }
+        }
+    } else {
+        //up is invalid
+        if ['|', 'J', 'L'].contains(&map[starting_coordinate.0 + 1][starting_coordinate.1]) {
+            //down is valid
+            if ['J', '7', '-'].contains(&map[starting_coordinate.0][starting_coordinate.1 + 1]) {
+                //right is valid
+                return 'F';
+            } else {
+                //left is valid
+                return '7';
+            }
+        } else {
+            // down is invalid
+            return '-';
+        }
+    }
 }
